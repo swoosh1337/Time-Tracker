d15fb605c3b983dd950c1277059f8a36d8dd0cdd
Irakli Grigolia <igrigolia@wpi.edu>
2023-12-01 15:13:13 -0500

    Fixed the issue. Should be working fine.

--- popup.js
+++ popup.js
@@ -26,10 +26,11 @@ function displayTime() {
 
 function aggregateTimes(timeSpent) {
   const promises = Object.keys(timeSpent).map(tabId => {
-    return new Promise((resolve, reject) => {
+    return new Promise(resolve => {
       chrome.tabs.get(parseInt(tabId), (tab) => {
-        if (chrome.runtime.lastError) {
-          reject(chrome.runtime.lastError);
+        if (chrome.runtime.lastError || !tab) {
+          // Skip this tab if an error occurs or tab doesn't exist
+          resolve(null);
         } else {
           resolve({ title: tab.title, time: timeSpent[tabId] });
         }
@@ -40,14 +41,15 @@ function aggregateTimes(timeSpent) {
   return Promise.all(promises)
     .then(results => {
       const aggregatedTimes = {};
-      results.forEach(({ title, time }) => {
-        const simplifiedTitle = title.split(' - ')[0]; // Adjust as needed
-        aggregatedTimes[simplifiedTitle] = (aggregatedTimes[simplifiedTitle] || 0) + time;
+      results.forEach(result => {
+        if (result) {
+          const { title, time } = result;
+          console.log(title)
+          const simplifiedTitle = title.split(' - ')[0]; // Adjust as needed
+          aggregatedTimes[simplifiedTitle] = (aggregatedTimes[simplifiedTitle] || 0) + time;
+        }
       });
       return aggregatedTimes;
-    })
-    .catch(error => {
-      console.error("Error aggregating times:", error);
-      return {};
     });
 }
+
